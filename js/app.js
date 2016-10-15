d601b67b37f57715ff05692b834ad1a64bfc8de5
Aaron Vontell <vontell@mit.edu>
2016-10-14 23:57:17 -0400

    Finish home page v1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,71 @@
 var app = angular.module("NearMissApp", ['ngRoute']);
+
+/*
+ * Navigation Controller
+ */
+app.controller('NavController', function() {
+    
+    angular.element(document).ready(function () {
+        $(".button-collapse").sideNav();
+    });
+    
+});
+
+/*
+ * Routing Configuration
+ */
+app.config(function($routeProvider) {
+    $routeProvider
+    	.when('/', {
+    		templateUrl: 'pages/home.html',
+            controller: 'HomeController'
+    	})
+        .when('/home', {
+    		templateUrl: 'pages/home.html',
+            controller: 'HomeController'
+    	})
+        .when('/report', {
+    		templateUrl: 'pages/form.html',
+            controller: 'ReportController'
+    	})
+        .when('/contact', {
+    		templateUrl: 'pages/contact.html',
+            controller: 'ContactController'
+    	})
+        .otherwise({
+            redirectTo: "/"
+        });
+});
+
+/*
+ * Home Controller
+ */
+app.controller('HomeController', function() {
+    
+    angular.element(document).ready(function () {
+        // Do something on startup
+    });
+    
+});
+
+/*
+ * Report Controller
+ */
+app.controller('ReportController', function() {
+    
+    angular.element(document).ready(function () {
+        // Do something on startup
+    });
+    
+});
+
+/*
+ * Contact Controller
+ */
+app.controller('ContactController', function() {
+    
+    angular.element(document).ready(function () {
+        // Do something on startup
+    });
+    
+});
