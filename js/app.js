b0617bc49cfe07d0fd20466605aec2d15883a36b
Aaron Vontell <vontell@mit.edu>
2016-10-16 13:54:34 -0400

    Add date and time pickers

--- js/app.js
+++ js/app.js
@@ -55,6 +55,18 @@ app.controller('ReportController', function() {
     
     angular.element(document).ready(function () {
         // Do something on startup
+        $('select').material_select();
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+          });
+        $('.timepicker').pickatime({
+            default: 'now',
+            twelvehour: true, // change to 12 hour AM/PM clock from 24 hour
+            donetext: 'OK',
+            autoclose: false,
+            vibrate: true // vibrate the device when dragging clock hand
+        });
     });
     
 });
